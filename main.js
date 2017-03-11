8c13579f460a010467df6aa7de37b64a63e18765
Aris <akoumis1@gmail.com>
2017-03-11 15:22:23 -0800

    async componentDidMount for App

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import Expo from 'expo';
+ import Expo, {Font, Components} from 'expo';
 import React from 'react';
 import {
   StyleSheet,
@@ -14,13 +14,40 @@ import {
 import Router from './navigation/Router';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      appIsReady: false  
+    }
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'roboto': require('./assets/fonts/roboto-black.ttf')
+    })  
+    console.log('WHAT')
+    this.setState({ appIsReady: true });
+  }
+
   render() {
+    if (!this.state.appIsReady) {
+      return <Components.AppLoading />
+    }
+
     return (
       <NavigationProvider router={Router}>
-        <StackNavigation initialRoute={Router.getRoute('home')} />
+        <StackNavigation initialRoute={Router.getRoute('list')} />
       </NavigationProvider>
     );
   }
+
+  // async _cacheResourcesAsync() {
+  //   await Font.loadAsync({
+  //     'roboto': require('./assets/fonts/roboto-black.ttf')
+  //   })
+  //   this.setState([appIsReady: true])
+  // }
+  
 }
 
 const styles = StyleSheet.create({
